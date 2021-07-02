34299802b8b9f4bd8ebe9e3016445addd78a59f9
Vladimir Kadalashvili <Kadalashvili.Vladimir@gmail.com>
2021-07-02 17:29:42 +0500

    Packet types

--- src/clj_mqtt/codec.clj
+++ src/clj_mqtt/codec.clj
@@ -21,5 +21,48 @@
   (gloss/compile-frame
    (gloss/bit-map :packet-type 4 :flags 4)))
 
+(def packet-types [
+                   [:reserved 0 :forbidden]
+                   [:connect 1 :client-server]
+                   [:connack 2 :server-client]
+                   [:publish 3 :both]
+                   [:puback 4 :both]
+                   [:pubrec 5 :both]
+                   [:pubrel 6 :both]
+                   [:pubcomp 7 :both]
+                   [:subscribe 8 :client-server]
+                   [:suback 9 :server-client]
+                   [:unsubscribe 10 :client-server]
+                   [:unsuback 11 :server-client]
+                   [:pingreq 12 :client-server]
+                   [:pingresp 12 :server-client]
+                   [:disconnect 14 :both]
+                   [:auth 15 :both]])
 
+(defn- packet-types-by-name- []
+  (let [pt packet-types]
+    (loop [fst (first pt)
+           rst (rest pt)
+           result {}]
+      (if (= 0 (count rst))
+        result
+        (recur 
+         (first rst)
+         (rest rst)
+         (assoc result (first fst) {:value (second fst), :direction (nth fst 2), :name (first fst)}))))))
 
+(def packet-types-by-name (packet-types-by-name-))
+
+(defn- packet-types-by-value- []
+  (let [pt packet-types]
+    (loop [fst (first pt)
+           rst (rest pt)
+           result {}]
+      (if (= 0 (count rst))
+        result
+        (recur
+         (first rst)
+         (rest rst)
+         (assoc result (second fst) {:value (second fst), :direction (nth fst 2), :name (first fst)}))))))
+
+(def packet-types-by-value (packet-types-by-value-))
