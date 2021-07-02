deb52021faa1be1db6c1e40252585a64ea0b18ff
Vladimir Kadalashvili <Kadalashvili.Vladimir@gmail.com>
2021-07-02 15:14:34 +0500

    Varint move to separate namespace

--- src/clj_mqtt/core.clj
+++ src/clj_mqtt/core.clj
@@ -1,15 +1,5 @@
 (ns clj-mqtt.core
-  (:gen-class)
-  (:require
-   [gloss.core :as gloss]
-   [gloss.io :as io]
-   [byte-streams :as bs]
-   [gloss.core.protocols :as protocols]
-   [gloss.data.bytes :as db]
-   [gloss.core.formats :as f]
-   [gloss.data.bytes.core :as corebytes])
-  (:import
-   [java.nio Buffer ByteBuffer]))
+  (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -18,76 +8,6 @@
 
 
 
-(defn- div [a b]
-  (int (/ a b)))
-
-
-(defn- encode-byte [n]
-  (let [x (div n 128)
-        eb (mod n 128)]
-    (if (> x 0)
-      [x (bit-or eb 128)]
-      [x eb])))
-
-(defn integer->variant [n]
-  (let [first-encoding (encode-byte n)]
-    (loop [x (first first-encoding)
-           eb (last first-encoding)
-           result [eb]]
-      (if (= x 0)
-        result
-        (let [enc (encode-byte x)]
-          (recur (first enc) (last enc) (conj result (last enc))))))))
-
-
-
-(defn variant->integer [bs]
-  (loop [multiplier 1
-         encoded-byte (first bs)
-         remaining-bytes (rest bs)
-         value (* multiplier (bit-and encoded-byte 127))]
-    (if (= 0 (bit-and encoded-byte 128))
-      value
-      (let [mul (* multiplier 128)
-            eb (first remaining-bytes)
-            rb (rest remaining-bytes)
-            v (+ value (* mul (bit-and eb 127)))]
-        (recur mul eb rb v)))))
-
-
-
-
-(defn first-byte [buf-seq]
-  (.get (first (db/take-bytes buf-seq 1))))
-
-
-
-(defn rest-bytes [buf-seq]
-  (db/drop-bytes buf-seq 1))
-
-(def varint
-  (reify
-    protocols/Writer
-    (sizeof [_] nil)
-    (write-bytes [_ buf val]
-      (let [bs (map unchecked-byte (integer->variant val))]
-        (if-not buf
-          (f/to-byte-buffer bs)
-          (corebytes/write-to-buf bs buf))))
-        protocols/Reader
-        (read-bytes [this buf-seq]
-            (let [buf-seq (db/dup-bytes buf-seq)]
-              (loop [multiplier 1
-                     encoded-byte (first-byte buf-seq)
-                     remaining-bytes (rest-bytes buf-seq)
-                     value (* multiplier (bit-and encoded-byte 127))]
-                (if (= 0 (bit-and encoded-byte 128))
-                  [true value remaining-bytes]
-                  (let [mul (* multiplier 128)
-                        eb (first-byte remaining-bytes)
-                        rb (rest-bytes remaining-bytes)
-                        v (+ value (* mul (bit-and eb 127)))]
-                    (recur mul eb rb v))))))))
 
 
 
--- test/clj_mqtt/core_test.clj
+++ test/clj_mqtt/core_test.clj
@@ -1,41 +1,6 @@
 (ns clj-mqtt.core-test
-  (:require [clojure.test :refer :all]
-            [clj-mqtt.core :refer :all]
-            [gloss.io :as io]
-            [gloss.core :as gloss]
-            [gloss.core.formats :as f]
-            [gloss.data.bytes.core :as bs]
-            [byte-streams]
-            [gloss.data.bytes :as db]))
+  (:require [clojure.test :refer :all]))
 
 
-(deftest test-integer->variant
-  (testing "Integer to VBI conversion"
-    (is (= [32] (integer->variant 32)))
-    (is (= [128 1] (integer->variant 128)))
-    (is (= [128 128 1] (integer->variant 16384)))
-    (is (= [128 128 128 1] (integer->variant 2097152)))))
-
-
-(deftest test-variant->integer
-  (testing "VBI to integer conversion"
-    (is (= 32 (variant->integer [32])))
-    (is (= 128 (variant->integer [128 1])))
-    (is (= 16384 (variant->integer [128 128 1])))
-    (is (= 2097152 (variant->integer [128 128 128 1])))))
-
-
-
-(deftest test-varint-codec
-  (testing "VBI decode"
-    (is (= 32 (io/decode varint (f/to-byte-buffer (map unchecked-byte [32])))))
-    (is (= 128 (io/decode varint (f/to-byte-buffer (map unchecked-byte [128 1])))))
-    (is (= 16384 (io/decode varint (f/to-byte-buffer (map unchecked-byte [128 128 1])))))
-    (is (= 2097152 (io/decode varint (f/to-byte-buffer (map unchecked-byte [128 128 128 1]))))))
-  (testing "VBI encode"
-    (is (byte-streams/bytes= (f/to-byte-buffer (map unchecked-byte [32])) (io/encode varint 32) ))
-    (is (byte-streams/bytes= (f/to-byte-buffer (map unchecked-byte [128 1]))   (io/encode varint 128)))
-    (is (byte-streams/bytes= (f/to-byte-buffer (map unchecked-byte [128 128 1])) (io/encode varint 16384)))
-    (is (byte-streams/bytes= (f/to-byte-buffer (map unchecked-byte [128 128 128 1])) (io/encode varint 2097152)))))
 
   
