926ac1f2e353c1aef94eca65bfda9a2283e7b412
Vladimir Kadalashvili <Kadalashvili.Vladimir@gmail.com>
2021-07-02 16:41:10 +0500

    First byte fixed header

--- src/clj_mqtt/codec.clj
+++ src/clj_mqtt/codec.clj
@@ -1,11 +1,25 @@
 (ns clj-mqtt.codec
   (:require
    [gloss.core :as gloss]
+   [gloss.core.formats :as f]
+   [gloss.io :as io]
    [clj-mqtt.varint :refer [varint]]))
 
+;;;primitives
 (def two-byte-int :uint16-be)
+
 (def four-byte-int :uint32-be)
+
 (def string (gloss/finite-frame two-byte-int (gloss/string :utf-8)))
+
 (def binary
   (gloss/finite-block two-byte-int))
+
+
+;;;fixed header
+(def fixed-header-first-byte 
+  (gloss/compile-frame
+   (gloss/bit-map :packet-type 4 :flags 4)))
+
+
 
